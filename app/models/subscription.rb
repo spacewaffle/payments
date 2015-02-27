d0d766ad79e593f84090c36e27851513769c2e20
Jon Cheng <jon.syndicate@gmail.com>
2015-02-27 00:31:54 -0500

    removed turbolinks and added more accessors

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,6 +7,6 @@ class Subscription < ActiveRecord::Base
   belongs_to :plan
 
   attr_accessible :cards, :stripe_id, :plan_id, :last_four, :coupon_id, 
-                  :card_type, :current_price
-                  
+                  :card_type, :current_price, :card_token, :credit_card_token
+
 end
