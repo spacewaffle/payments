28aeef461907b1c353b4c505545022514d13c3d2
Jon Cheng <jon.syndicate@gmail.com>
2015-02-27 00:32:32 -0500

    removed unnecessary attr accessors

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,7 +6,7 @@ class Subscription < ActiveRecord::Base
   belongs_to :coupon
   belongs_to :plan
 
-  attr_accessible :cards, :stripe_id, :plan_id, :last_four, :coupon_id, 
-                  :card_type, :current_price, :card_token, :credit_card_token
+  attr_accessible :stripe_id, :plan_id, :last_four, :coupon_id, 
+                  :card_type, :current_price, :credit_card_token
 
 end
